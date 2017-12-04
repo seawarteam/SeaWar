2f74a5b48c7ea17ea444df719a7474313785e00f
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2017-12-04 23:45:58 +0100

    Update FenetrePrincipale.java

--- ProjetSeaWar_V1.0/src/FenetrePrincipale.java
+++ ProjetSeaWar_V1.0/src/FenetrePrincipale.java
@@ -1,10 +1,10 @@
-package affichage;
-
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class FenetrePrincipale extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -12,17 +12,18 @@ public class FenetrePrincipale extends JFrame {
 	public final static int nBoutonsHaut = 3; //différent de zéro, nombre de boutons en haut
 	public final static int largeurMenuHaut = 2; //approximativement la largeur du menu en haut en pourcentage
 	public final static int largeurMenuGauche = 30; //approximativement la largeur du menu a gauche en pourcentage
-	public final static int nCasesX = 19;
-	public final static int nCasesY = 6;
+	public final static int nCasesX = 30;
+	public final static int nCasesY = 20;
 
-	private static int longueurCote=80;	//distance le centre et un point = longueur d'un cote
+	private static int longueurCote;	//distance le centre et un point = longueur d'un cote
 	private static int apotheme; //apotheme = distance entre le centre et le milieu d'un cote
 	private static int resteX;	//resteX : longueur du 'triangle' sur un cote de l'hexagone
 
 	private static Graphics2D cg;
 
 	private static PolyCar tab[][] = new PolyCar[nCasesX][nCasesY];
-	
+	private JPanel plateau;
+	private JScrollPane scroll;
 
 
 	public FenetrePrincipale() {
@@ -30,10 +31,8 @@ public class FenetrePrincipale extends JFrame {
 		this.setExtendedState(MAXIMIZED_BOTH); // La fenetre est créée en plein ecran
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //TODO Demander a sauvegarder en fermant?
 
-		double dlcote = longueurCote;
-		apotheme = (int) (dlcote * (Math.sqrt(3)/2)); 
-		double dapotheme = apotheme;
-		resteX =(int) (dapotheme - (dlcote/4));
+		setTailleHex(30);
+		initTabHex();
 
 		JPanel panPrincipal = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -59,9 +58,10 @@ public class FenetrePrincipale extends JFrame {
 		gbc.weighty = 100;
 		gbc.gridx = nBoutonsHaut;
 		gbc.gridheight = 2;
-		JPanel menuGauche = new JPanel(new GridLayout(1,3));
+		JPanel menuGauche = new JPanel(new GridLayout(5,1));
+		initSliderTailleHex(menuGauche);
 		panPrincipal.add(menuGauche, gbc);
-
+		
 		gbc.weightx = 100-largeurMenuGauche;
 		gbc.weighty = 100-largeurMenuHaut;
 		gbc.gridx = 0;
@@ -70,8 +70,8 @@ public class FenetrePrincipale extends JFrame {
 		gbc.gridwidth = nBoutonsHaut;
 
 
-		DrawingPanel plateau = new DrawingPanel();
-		JScrollPane scroll = new JScrollPane(plateau);
+		plateau = new DrawingPanel();
+		scroll = new JScrollPane(plateau);
 		scroll.getVerticalScrollBar().setUnitIncrement(2*apotheme);
 		scroll.getHorizontalScrollBar().setUnitIncrement(resteX+longueurCote);
 
@@ -83,6 +83,36 @@ public class FenetrePrincipale extends JFrame {
 
 	}	
 
+	public void setTailleHex(int tailleCote) {
+		longueurCote = tailleCote;
+		double dlcote = longueurCote;
+		apotheme = (int) (dlcote * (Math.sqrt(3)/2)); 
+		double dapotheme = apotheme;
+		resteX =(int) (dapotheme - (dlcote/4));	
+	}
+	
+	public void initSliderTailleHex(JPanel panel) {
+		JSlider slider = new JSlider();
+		slider.setMaximum(100);
+		slider.setMinimum(10);
+		slider.setValue(longueurCote);
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				int valeur = slider.getValue();
+				setTailleHex(valeur);
+				scroll.repaint();
+				scroll.getVerticalScrollBar().setUnitIncrement(2*apotheme);
+				scroll.getHorizontalScrollBar().setUnitIncrement(resteX+longueurCote);
+				plateau.setPreferredSize(new Dimension(nCasesX*(resteX+longueurCote)+resteX,(2*apotheme)*nCasesY+apotheme));
+				scroll.revalidate();
+			}
+		});
+		
+		
+		panel.add(slider);
+	}
+	
+	
 	public static class PolyCar{ //combinaison d'un polygone , une cooleur et plus peut etre
 		public Polygon poly;
 		public Color col;
@@ -107,7 +137,6 @@ public class FenetrePrincipale extends JFrame {
 	// Le Panel du plateau d'hexagones
 	class DrawingPanel extends JPanel {
 		private static final long serialVersionUID = 1L;
-		private int deb=0;
 		public DrawingPanel() {
 			setPreferredSize(new Dimension(nCasesX*(resteX+longueurCote)+resteX,(2*apotheme)*nCasesY+apotheme));
 			setBackground(Color.BLACK);
@@ -122,7 +151,7 @@ public class FenetrePrincipale extends JFrame {
 				if (p.x < 0 || p.y < 0 || p.x >= nCasesX || p.y >= nCasesY) return;
 				if(tab[p.x][p.y].col==Color.GRAY) {tab[p.x][p.y].col=Color.CYAN;}
 				else {tab[p.x][p.y].col=Color.GRAY;}
-				repaint();
+				plateau.repaint();
 			}		
 
 		}
@@ -133,39 +162,35 @@ public class FenetrePrincipale extends JFrame {
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //permet d'éviter des effets de bords moches
 			super.paintComponent(g);
 			cg = g2;
-			if(deb==0) {
-				for (int i=0;i<nCasesX;i++) {	//TODO Cela ne marche que pour une grille carrée. Créer une méthode si le type de grille change
-					for (int j=0;j<nCasesY;j++) {
-						drawHex(i,j,g2);
-					}
+			for (int i=0;i<nCasesX;i++) {	//TODO Cela ne marche que pour une grille carrée. Créer une méthode si le type de grille change
+				for (int j=0;j<nCasesY;j++) {
+					drawHex(i,j,g2);
 				}
-				deb++;
-			} else {
-				for (int i=0;i<nCasesX;i++) {	//TODO Cela ne marche que pour une grille carrée. Créer une méthode si le type de grille change
-					for (int j=0;j<nCasesY;j++) {
-						fillHex(i,j);
-					}
-				}
+			}
+			
+		}
+	}
+	private void initTabHex() {
+		for (int i=0;i<nCasesX;i++) {
+			for (int j=0;j<nCasesY;j++) {
+				int x = i * (longueurCote+resteX);
+				int y = j * apotheme*2 + (i%2) * apotheme;
+				Polygon poly = hexagone(x,y);
+				tab[i][j]=new PolyCar(poly, Color.CYAN);
 			}
 		}
 	}
-
+	
+	
 	public static void drawHex(int i, int j, Graphics2D g2) { //Cee un hexagone en (i,j)
 		int x = i * (longueurCote+resteX);
 		int y = j * apotheme*2 + (i%2) * apotheme;
-		Polygon poly = hexagone(x,y);
-		tab[i][j]=new PolyCar(poly, Color.CYAN);
+		Polygon polyg = hexagone(x,y);
+		tab[i][j].poly = polyg;
 		g2.setColor(tab[i][j].col);
-		g2.fillPolygon(poly);
+		g2.fillPolygon(polyg);
 		g2.setColor(Color.BLACK);
-		g2.drawPolygon(poly);
-	}
-
-	public static void fillHex(int i, int j) { //remplit avec la lettre n et couleur
-		cg.setColor(tab[i][j].col);
-		cg.fillPolygon(tab[i][j].poly);
-		cg.setColor(Color.BLACK);
-		cg.drawPolygon(tab[i][j].poly);
+		g2.drawPolygon(polyg);
 	}
 
 	public static Point pxtoHex(int mx, int my) { //on a clique sur le pixel (mx,my) et on renvoie le polygone correspondant
