9691230ac3a6cb5446e0f5e4decb753765ca7b50
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2017-12-05 18:19:36 +0100

    Update Eau.java

--- ProjetSeaWar_V1.0/src/Eau.java
+++ ProjetSeaWar_V1.0/src/Eau.java
@@ -8,6 +8,10 @@ public class Eau extends Case {
 
     private Boolean estOccupe;
     
+    public static final Color couleurVide = Color.CYAN;
+    public static final Color couleurDep = Color.BLUE;
+    public static final Color couleurTir = Color.GRAY;
+    
     public Eau() {
         super(); // Il faut qu'il y ait un constructeur de case sans argument
         estOccupe = false;
