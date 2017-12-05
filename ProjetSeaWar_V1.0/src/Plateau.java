e0947bbf3671b6ae221d4a108bb83f317b7ddc1e
Byrishyn <33869863+Byrishyn@users.noreply.github.com>
2017-12-05 17:39:27 +0100

    Update Plateau.java

--- ProjetSeaWar_V1.0/src/Plateau.java
+++ ProjetSeaWar_V1.0/src/Plateau.java
@@ -5,16 +5,23 @@ import java.util.*;
  * 
  */
 public class Plateau {
-
-    /**
-     * Default constructor
-     */
-    public Plateau() {
+    
+    private Set<Case> cases;
+    private int largeur;
+    private int longueur;
+    
+    public Plateau(int l, int L) {
+        cases = new HashSet<Case>;
+        largeur = l;
+        longueur = L;
+        for (int i = 0 ; i < l ; i++) {
+            for (int j = 0 ; j < L ; j++) { 
+                cases.add(new Eau(i, j));
+                
+              
+        
+            
     }
 
-    /**
-     * 
-     */
-    public Set<Case> cases;
 
 }
