a6219a8f5354e9f5b7df48fc21b979b4fca1c213
Byrishyn <33869863+Byrishyn@users.noreply.github.com>
2017-12-05 17:00:18 +0100

    Update Eau.java

--- ProjetSeaWar_V1.0/src/Eau.java
+++ ProjetSeaWar_V1.0/src/Eau.java
@@ -6,10 +6,23 @@ import java.util.*;
  */
 public class Eau extends Case {
 
-    /**
-     * Default constructor
-     */
+    private Boolean estOccupe;
+    
     public Eau() {
+        super(); // Il faut qu'il y ait un constructeur de case sans argument
+        estOccupe = false;
+    }
+    
+    public Boolean getEstOccupe(){
+        return estOccupe;  
+    }
+    
+    public void freeCase(){
+        estOccupe = false;   
+    }
+    
+    public void takeCase(){
+        estOccupe = true;   
     }
 
 }
