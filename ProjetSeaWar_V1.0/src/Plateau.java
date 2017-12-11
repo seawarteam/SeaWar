bbd6c8e547a21fe6958bca9131acdbc6c719aa1d
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2017-12-11 18:28:55 +0100

    Update Plateau.java

--- ProjetSeaWar_V1.0/src/Plateau.java
+++ ProjetSeaWar_V1.0/src/Plateau.java
@@ -8,7 +8,7 @@ public class Plateau {
     private int longueur;
     
     public Plateau(int l, int L) {
-        cases = new HashSet<Case>;
+        cases = new HashSet<Case>();
         largeur = l;
         longueur = L;
         for (int i = 0 ; i < l ; i++) {
@@ -17,7 +17,7 @@ public class Plateau {
             }
         }           
               
-        //TO DO : pouvoir retourner un set d'obstacles (tout les obstacles présents) 
+        //TO DO : pouvoir retourner un set d'obstacles (tout les obstacles prÃ©sents) 
         //--> faire une liste "fixe" avec les rochers et une autre "changeante" avec les bateaux.
             
     }
