3ccba7cb122616ff7198ae00b4d554edbe32aede
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2017-12-16 20:16:27 +0100

    Update Rocher.java

--- SeaWar-master/ProjetSeaWar_V1.0/src/partie/Rocher.java
+++ SeaWar-master/ProjetSeaWar_V1.0/src/partie/Rocher.java
@@ -12,12 +12,14 @@ public class Rocher extends Case {
     public static final Color couleurVide = Color.YELLOW;
     public static final Color couleurTir = Color.GRAY;
 	
-    public Rocher(Polygon p, int i, int j) {
+    public Rocher(Polygon p, int i, int j,Observer obs) {
+    	super();
     	poly = p;
     	estOccupe = false;
     	takePosition = null;
 		col = couleurVide;
 		position = Position.getPosition(i, j);
+		addObserver(obs);
     }
     
     public boolean getEstOccupe(){
@@ -31,6 +33,16 @@ public class Rocher extends Case {
 		}
 	}
     
+    public String toString() {
+    	return "Terre";
+    }
+    
+	public void getInfo() {
+    	setChanged();
+    	notifyObservers(this);
+    	clearChanged();
+	}
+	
 	public void takeCase(Navire n){
 		//Ne rien faire
 	}
