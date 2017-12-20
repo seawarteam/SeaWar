1ec343e07ac41e34b7d636d8b13b5e70091c190e
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2017-12-20 15:48:57 +0100

    Update FenetrePrincipale.java

--- SeaWar-master/ProjetSeaWar_V1.0/src/fenetre/FenetrePrincipale.java
+++ SeaWar-master/ProjetSeaWar_V1.0/src/fenetre/FenetrePrincipale.java
@@ -217,6 +217,10 @@ public class FenetrePrincipale extends JFrame implements Observer{
 		if(p.currentJ.getNavEtatCourant()==null) {
 			actionsBateau.setVisible(false);
 		}
+		if(p.gagne){
+			Gagne gg = new Gagne();
+			
+		}
 	}
 	
 
@@ -329,6 +333,13 @@ public class FenetrePrincipale extends JFrame implements Observer{
 					drawHex(i,j,g2);
 				}
 			}
+		}
+	}
+	
+	class Gagne extends JOptionPane{
+		public Gagne(){
+			super();
+			showMessageDialog(this, "Victoire de "+ partie.currentJ.getNom(), "Bravo !", JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 	
