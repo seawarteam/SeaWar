2a3d41d93dba4c308349d17484ba0dbfb189655a
LMare <33813639+LMare@users.noreply.github.com>
2018-01-08 17:20:23 +0100

    Add files via upload

--- SeaWar-master/ProjetSeaWar_V1.0/src/partie/ControleurModif.java
+++ SeaWar-master/ProjetSeaWar_V1.0/src/partie/ControleurModif.java
@@ -1,7 +1,10 @@
 package partie;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import javax.swing.ListModel;
 
 import etatModif.*;
 
@@ -20,8 +23,56 @@ public class ControleurModif {
 
 	public void hexClique(Position pos) {
 		etat.clique(pos, this);
-
 	}
+	
+	public void demandeModifMap(){
+		//Sauvegarder la carte
+		getEditeur().resetPlateau();
+		//Débloquer les boutons (eau, phare, rocher)
+		etat.modifMap(this);
+	}
+	
+	public Editeur getEditeur(){
+		return editeur;
+	}
+
+	public void demandeAjoutEau() {
+		etat.modifMap(this);
+	}
+	
+	public void demandeAjoutRocher() {
+		etat.modifMap(this);		
+	}
+	
+	public void demandeAjoutPhare() {
+		etat.modifMap(this);		
+	}
+
+	public void demandeModifCanonP() {
+		getEditeur().resetPlateau();
+		System.out.println("1111");
+		etat = EditCanonP.getEtat();
+		
+	}
+	
+	public void demandeModifCanonS() {
+		getEditeur().resetPlateau();
+		etat = EditCanonS.getEtat();
+		
+	}
+
+	public void demandeAjoutBase(String str) {
+		setEtat(EditBase.getEtat());
+		etat.setCurrentJ(str);
+	}
+
+	
+	
+
+	
+	
+
+	
 
 	
 
