07a8eeae7c3130ced0498d3100872059793259d2
LMare <33813639+LMare@users.noreply.github.com>
2018-01-11 14:25:13 +0100

    Add files via upload

---
+++ SeaWar-master/ProjetSeaWar_V1.0/src/etatModif/EditNavire.java
@@ -0,0 +1,43 @@
+package etatModif;
+
+import partie.ControleurModif;
+import partie.Position;
+
+public class EditNavire implements EtatModif{
+	
+	private static EtatModif etat = new EditNavire();
+	
+	private EditNavire() {
+		super();
+	}
+
+	public static EtatModif getEtat(){
+		return etat ;
+	}
+
+	public void clique(Position pos, ControleurModif c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modifMap(ControleurModif c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modifCanonP(ControleurModif c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modifCanonS(ControleurModif c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCurrentJ(String str) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
