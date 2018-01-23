8b9d8a396e5e639ae217b1f35117681ad6c84820
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2018-01-23 22:34:31 +0100

    Update EtatInit.java

--- ProjetSeaWar_V2.0/src/etatActivite/EtatInit.java
+++ ProjetSeaWar_V2.0/src/etatActivite/EtatInit.java
@@ -1,5 +1,6 @@
 package etatActivite;
 
+import etat.Detruit;
 import partie.Case;
 import partie.Controleur;
 import partie.Navire;
@@ -8,18 +9,22 @@ import partie.Position;
 public class EtatInit implements EtatAction{
 
 	private static EtatInit e = new EtatInit();
-	
+
 	private EtatInit() {	
 	}
-	
+
 	public void clique(Position pos, Controleur c) {
 		Navire n = c.getPartie().getNavOnPos(pos);
 		if(n != null){
-			if(n.getNomJ().equals(c.getPartie().getCurrentJ().getNom())){
-				c.getPartie().getCurrentJ().setCurrentN(n);
-				n.getPrivateInfo();
+			if(!(n.getEtatCourant() instanceof Detruit)) {
+				if(n.getNomJ().equals(c.getPartie().getCurrentJ().getNom())){
+					c.getPartie().getCurrentJ().setCurrentN(n);
+					n.getPrivateInfo();
+				}
+				n.getInfo();
 			}
-			n.getInfo();
+
+
 		}else{
 			Case cases = c.getPartie().getCaseOnPos(pos);
 			cases.getInfo();
