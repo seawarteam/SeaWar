e0da852f79d2b30d6516780e4aa5cdad15c55a07
nkerkhof <30441875+nkerkhof@users.noreply.github.com>
2018-01-24 12:31:20 +0100

    Update Phare.java

--- ProjetSeaWar_V2.0/src/partie/Phare.java
+++ ProjetSeaWar_V2.0/src/partie/Phare.java
@@ -5,7 +5,7 @@ import java.awt.Polygon;
 import java.io.Serializable;
 import java.util.*;
 public class Phare extends Eau implements Serializable {
-
+	private static final long serialVersionUID = 3548840552347911252L;
 	public static final Color couleurVide = Color.WHITE;
 	public Color couleurOccupeDef = Color.RED;
 	public static final Color couleurOri = new Color(0, 255, 0);
